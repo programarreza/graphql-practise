20baf73d6f4baf8e94027750b494240c0399b28e
programarreza <programarreza@gmail.com>
2025-01-14 09:54:35 +0600

    products & single product get done

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { db } from './db';
 
 const typeDefs = `
-  type Book {
-    title: String
-    author: String
-  }
-
+	type Product {
+		id: ID!
+		name: String
+		image: String
+		description: String
+		price: Float
+		quantity: Int
+		onStock: Boolean
+		categoryId: String
+	}
     type Query {
-    books: [Book]
+    products: [Product]
+	product(productId: ID!): Product 
   }
 `;
 
-const books = [
-	{
-		title: 'The Awakening',
-		author: 'Kate Chopin',
-	},
-	{
-		title: 'City of Glass',
-		author: 'Paul Auster',
-	},
-];
 
 const resolvers = {
 	Query: {
-		books: () => books,
+		products: () => db.products,
+		product: (parent: any, args: { productId: string }, context: any) => {
+			const result = db.products.find(product => product.id === args.productId)
+			return result
+		}
 	},
 };
 
